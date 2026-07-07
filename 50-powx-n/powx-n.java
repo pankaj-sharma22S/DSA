3b67976c69d17fc21dda5560649900040079a0a9
pankaj-sharma22S <ps.hp22.4@gmail.com>
2026-07-07 14:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (11.41%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+     double ans=1;
+     long N=n;
+     if(N<0){
+        N=-N;
+        x=1/x;
+     }
+     while(N>0){
+        if(N%2==1){
+            ans*=x;
+        }
+        x*=x;
+        N=N/2;
+     }
+     return ans;
+    }
+}
