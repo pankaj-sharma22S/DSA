b3d9fc29295579e1278d220dae8c4853d70ef7c2
pankaj-sharma22S <ps.hp22.4@gmail.com>
2026-08-25 13:00:26 +0530

    Time: 6 ms (93.22%) | Memory: 134.1 MB (60.53%) - LeetSync

---
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+        int[][] arr=new int[m][n];
+        int left=0,right=n-1,top=0,bottom=m-1;
+       ListNode temp=head;
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(arr[i], -1);
+        }
+        while(temp!=null&&top<=bottom&& left<=right){
+        
+            for(int i=left; i<=right && temp != null; i++){
+                
+                arr[top][i]=temp.val;                                temp=temp.next;
+               
+
+            }
+            top++;
+            for(int i=top; i<=bottom && temp != null; i++){
+                  arr[i][right]=temp.val;                               temp=temp.next;
+
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right; i>=left && temp != null; i--){
+                  arr[bottom][i]=temp.val;
+                temp=temp.next;
+            }
+            }
+            bottom--;
+            if(left<=right){
+                for(int i=bottom; i>=top && temp != null; i--){
+                  arr[i][left]=temp.val;
+             temp=temp.next;
+            }
+            }
+            left++;
+            
+        }    
+return arr;
+        
+    }
+}
